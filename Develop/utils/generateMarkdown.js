7fe9b0dc9cc01d7bde04cae8633de1ddcb26b77f
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-08-25 18:56:25 -0500

    testing

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -26,10 +26,47 @@ This project is licensed under the ${license} license.`
 };
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const markdownTemplate = (data) => {
+  return `# ${data.projectTitle}
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
 
-`;
-}
 
-module.exports = generateMarkdown;
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  * [Contributing](#contributing)
+  * [Questions](#questions)
+  
+  
+
+  ## Installation
+  To install all necessary dependencies, run the following command:
+  \`\`\`
+  ${data.installation}
+  
+
+  ## Usage
+  ${data.usage}
+  ${renderLicenseSection(data.license)}
+
+
+  ## Contributing
+  ${data.contributing}
+
+
+  ## Tests
+  To run tests, run the following command:
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  
+  ## Questions
+  If you have any questions about this repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.githubUsername}](https://github.com/${data.githubUsername}).
+  `;
+};
+
+module.exports = markdownTemplate;
+
